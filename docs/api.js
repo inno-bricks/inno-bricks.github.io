a5aa4ff4a7e1cdd8bfac72f8b97cd33f2dd43fd7
jiangwy <294358991@qq.com>
2014-06-18 11:22:06 +0800

    add docs folder

---
+++ docs/api.js
@@ -0,0 +1,36 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "BricksUI",
+        "BricksUI.I18n",
+        "BricksUI.StateHandler",
+        "Ember.BuEditor"
+    ],
+    "modules": [
+        "BricksUI",
+        "BricksUI-I18n",
+        "bricksui",
+        "bricksui-metal"
+    ],
+    "allModules": [
+        {
+            "displayName": "BricksUI",
+            "name": "BricksUI"
+        },
+        {
+            "displayName": "bricksui",
+            "name": "bricksui"
+        },
+        {
+            "displayName": "BricksUI-I18n",
+            "name": "BricksUI-I18n",
+            "description": "国际化支持"
+        },
+        {
+            "displayName": "bricksui-metal",
+            "name": "bricksui-metal",
+            "description": "BricksUI ,a widget library on ember.js"
+        }
+    ]
+} };
+});
